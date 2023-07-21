b90c2799f8cef6f7ad6cfdd19824538c1c794060
Michele Pardo <miguelalfonsopardo@gmail.com>
2023-07-21 21:07:26 +0200

    show main temp

--- lib/features/weather/weather_screen.dart
+++ lib/features/weather/weather_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/features/weather/bloc/weather_bloc.dart';
+import 'package:weather_app/models/weather_list_element.dart';
 import '../../design/widgets/language_switcher/language_switcher.dart';
 import '../../design/widgets/navigation_bar/bottom_navigation_bar.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({Key? key}) : super(key: key);
 
+  static double kelvinToCelsius(double kelvin) => kelvin - 273.15;
+
   @override
   Widget build(BuildContext context) {
-
     BlocProvider.of<WeatherBloc>(context, listen: false)
         .add(LoadWeatherEvent());
 
@@ -21,23 +23,33 @@ class WeatherScreen extends StatelessWidget {
               child: LanguageSwitcher(),
               alignment: Alignment.topRight,
             ),
-            BlocBuilder<WeatherBloc, WeatherState>(
-              builder: (context, state) {
-                return Center(
-                  child: Text(
-                    state.weatherResponse.cod,
-                    style: const TextStyle(
-                      color: Colors.black,
-                    ),
+            BlocBuilder<WeatherBloc, WeatherState>(builder: (context, state) {
+              if (state is WeatherLoadingState) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.black12,
                   ),
                 );
-              },
-            )
+              } else {
+                return body(state.weatherResponse.list.first);
+              }
+            })
           ],
         ),
       ),
       extendBody: true,
       bottomNavigationBar: const CitiesBottomNavigationBar(),
+    );
+  }
+
+  Widget body(WeatherListElement weather) {
+    final String mainTemp = "${kelvinToCelsius(weather.main.temp).round()}";
+
+    return Center(
+      child: Text(mainTemp, style: const TextStyle(
+        fontSize: 100,
+        letterSpacing: 2.1
+      ),),
     );
   }
 }
